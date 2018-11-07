eb253fe7d69ae18469044ba2535eccdd30c2274e
Udayagiri Sankaranarayana (BLR GSS) <sankaranarayanau@nousinfo.com>
2018-11-07 11:35:32 +0530

    created new component

---
+++ src/app/components/new/new.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as interact from 'interactjs';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.css']
+})
+export class NewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // var movable = document.querySelector('#movable-box');
+
+
+    // interact(movable)
+    //   .draggable({
+    //     allowFrom: '.drag-handle',
+    //     onmove: function (event) {
+          
+    //     }
+    //   })
+    //   .resizable({
+    //     allowFrom: '.resize-handle',
+    //     onmove: function (event) {
+          
+    //     }
+    //   })
+    //   .pointerEvents({
+    //     allowFrom: '',
+    //   });;
+    
+
+  }
+
+}
